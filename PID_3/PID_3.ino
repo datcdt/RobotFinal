147c913df923fc3cd03cc74282fedc1006f6125a
datcdt <124320841+datcdt@users.noreply.github.com>
2023-05-26 09:53:17 +0700

    code PID

--- PID_3/PID_3.ino
+++ PID_3/PID_3.ino
@@ -1,5 +1,5 @@
+#include <TimerOne.h>
 #include <Wire.h>
-#include <PID_v1.h>
 #include <ros.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Float64.h>
@@ -11,22 +11,22 @@
 
 
 //initializing all the variables
-#define LOOPTIME          100                 //Looptime in millisecond
+#define LOOPTIME          100                 //Thời gian lấy mẫu
 const byte noCommLoopMax = 10;                //number of main loops the robot will execute without communication before stopping
 unsigned int noCommLoops = 0;                 //main loop without communication counter
+int SamplingTime = 0.1; //giaya
 
+const int PIN_L_IN1 = 23;
+const int PIN_L_IN2 = 25;
+const int PIN_L_PWM = 4;
+const int PIN_L_ENCOD_A_MOTOR = 2;               //A channel for encoder of left motor
+const int PIN_L_ENCOD_B_MOTOR = 16;               //B channel for encoder of left motor
 
-const int PIN_L_IN1 = 29;
-const int PIN_L_IN2 = 31;
-const int PIN_L_PWM = 5;
-const int PIN_L_ENCOD_A_MOTOR = 3;               //A channel for encoder of left motor
-const int PIN_L_ENCOD_B_MOTOR = 17;               //B channel for encoder of left motor
-
-const int PIN_R_IN1 = 23;
-const int PIN_R_IN2 = 25;
-const int PIN_R_PWM = 4;
-const int PIN_R_ENCOD_A_MOTOR = 2;              //A channel for encoder of right motor
-const int PIN_R_ENCOD_B_MOTOR = 16;              //B channel for encoder of right motor
+const int PIN_R_IN1 = 29;
+const int PIN_R_IN2 = 31;
+const int PIN_R_PWM = 5;
+const int PIN_R_ENCOD_A_MOTOR = 3;              //A channel for encoder of right motor
+const int PIN_R_ENCOD_B_MOTOR = 17;              //B channel for encoder of right motor
 
 unsigned long lastMilli = 0;
 const double radius = 0.026735;               // Bán kính bánh xe truyền động, m
@@ -35,59 +35,51 @@ const double wheelbase = 0.206;               //Khoảng cách giữa 2 bánh xe
 double speed_req = 0;                         //Vận tốc dài mong muốn từ ROS gửi xuống, m/s
 double angular_speed_req = 0;                 //Vận tốc góc mong muốn từ ROS gửi xuống, rad/s
 
+double kpL = 0.0, kiL = 7, kdL = 0;
 double speed_req_left = 0;                    //Vận tốc bánh trái mong muốn, m/s
 double speed_act_left = 0;                    //Vận tốc bánh trái thực tế, m/s
-double speed_cmd_left = 0;                    //Vận tốc bánh trái cần thiết(command), m/s
+double left_error = 0;
+double left_lastError = 0;
+double left_llastError = 0;
+int PWM_leftMotor_last = 0; 
+int PWM_leftMotor = 0;                         //Xung PWM cấp cho  động cơ trái
+double aL = 0,bL = 0,gL = 0;
+volatile float pos_left = 0;       //biến đếm xung encoder động cơ trái
 
+double kpR = 0.0, kiR = 14.7, kdR = 0;
 double speed_req_right = 0;                   //Desired speed for right wheel in m/s
 double speed_act_right = 0;                   //Actual speed for right wheel in m/s
-double speed_cmd_right = 0;                   //Command speed for right wheel in m/s
+double right_error = 0;
+double right_lastError = 0;
+double right_llastError = 0;
+int PWM_rightMotor_last = 0; 
+int PWM_rightMotor = 0;                       //Xung PWM cấp cho  động cơ phải
+double aR = 0,bR = 0,gR = 0;
+volatile float pos_right = 0;      //biến đếm xung encoder động cơ phải
 
 const double max_speed = 0.25;                 //Vận tốc giới hạn, m/s
 
-int PWM_leftMotor = 0;                     //PWM command for left motor
-int PWM_rightMotor = 0;                    //PWM command for right motor
-
-unsigned int encoder_left = 0;
-unsigned int encoder_right = 0;
-
-
-// PID Parameters
-const double PID_left_param[] = { 0.29167, 189.8799, 0.0 }; //Respectively Kp, Ki and Kd for left motor PID
-const double PID_right_param[] = { 0.16331, 258.733, 0.0 }; //Respectively Kp, Ki and Kd for right motor PID
-
-volatile float pos_left = 0;       //Left motor encoder position
-volatile float pos_right = 0;      //Right motor encoder position
-
-// PID myPID(&input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT/REVERVE)
-PID PID_leftMotor(&speed_act_left, &speed_cmd_left, &speed_req_left, PID_left_param[0], PID_left_param[1], PID_left_param[2], DIRECT);          //Setting up the PID for left motor
-PID PID_rightMotor(&speed_act_right, &speed_cmd_right, &speed_req_right, PID_right_param[0], PID_right_param[1], PID_right_param[2], DIRECT);   //Setting up the PID for right motor
-
+int encoder_left = 0;
+int encoder_right = 0;
 
 ros::NodeHandle nh;
-
 //function that will be called when receiving command from host
 void handle_cmd (const geometry_msgs::Twist& cmd_vel) {
   noCommLoops = 0;                                                  //Reset the counter for number of main loops without communication
-
   speed_req = cmd_vel.linear.x;                                     //Extract the commanded linear speed from the message
-
   angular_speed_req = cmd_vel.angular.z;                            //Extract the commanded angular speed from the message
 
   speed_req_left  = speed_req - angular_speed_req * (wheelbase / 2); //Calculate the required speed for the left motor to comply with commanded linear and angular speeds
   speed_req_right = speed_req + angular_speed_req * (wheelbase / 2); //Calculate the required speed for the right motor to comply with commanded linear and angular speeds
-}
- 
+} 
 geometry_msgs::Pose2D pwm_output_msg; 
 geometry_msgs::Pose2D encoder_msg; 
 geometry_msgs::Vector3Stamped speed_msg;                                //create a "speed_msg" ROS message
 
 ros::Subscriber<geometry_msgs::Twist> cmd_vel("cmd_vel", handle_cmd);   //create a subscriber to ROS topic for velocity commands (will execute "handle_cmd" function when receiving data)
-
 ros::Publisher speed_pub("speed", &speed_msg);                          //create a publisher to ROS topic "speed" using the "speed_msg" type
 ros::Publisher pwm_output_pub("pwm_output", &pwm_output_msg);
 ros::Publisher encoder_pub("encoder", &encoder_msg);
-
 
 void setup() {
   nh.initNode();                            //init ROS node
@@ -97,7 +89,6 @@ void setup() {
   nh.advertise(encoder_pub);
   nh.advertise(pwm_output_pub);
 
-
   //setting motor speeds to zero
   pinMode(PIN_L_IN1, OUTPUT);
   pinMode(PIN_L_IN2, OUTPUT);
@@ -106,67 +97,54 @@ void setup() {
   pinMode(PIN_R_IN1, OUTPUT);
   pinMode(PIN_R_IN2, OUTPUT);
   pinMode(PIN_R_PWM, OUTPUT);
-
-
-
-  //setting PID parameters
-  PID_leftMotor.SetSampleTime(95);
-  PID_rightMotor.SetSampleTime(95);
-  PID_leftMotor.SetOutputLimits(-max_speed, max_speed);
-  PID_rightMotor.SetOutputLimits(-max_speed, max_speed);
-  PID_leftMotor.SetMode(AUTOMATIC);
-  PID_rightMotor.SetMode(AUTOMATIC);
-
   // Define the rotary encoder for left motor
-  pinMode(PIN_L_ENCOD_A_MOTOR, INPUT);
+  pinMode(PIN_L_ENCOD_A_MOTOR, INPUT_PULLUP);
   pinMode(PIN_L_ENCOD_B_MOTOR, INPUT);
-  digitalWrite(PIN_L_ENCOD_A_MOTOR, HIGH);                // turn on pullup resistor
-  digitalWrite(PIN_L_ENCOD_B_MOTOR, HIGH);
   attachInterrupt(digitalPinToInterrupt(PIN_L_ENCOD_A_MOTOR), encoderLeftMotor, RISING);
 
   // Define the rotary encoder for right motor
-  pinMode(PIN_R_ENCOD_A_MOTOR, INPUT);
+  pinMode(PIN_R_ENCOD_A_MOTOR, INPUT_PULLUP);
   pinMode(PIN_R_ENCOD_B_MOTOR, INPUT);
-  digitalWrite(PIN_R_ENCOD_A_MOTOR, HIGH);                // turn on pullup resistor
-  digitalWrite(PIN_R_ENCOD_B_MOTOR, HIGH);
   attachInterrupt(digitalPinToInterrupt(PIN_R_ENCOD_A_MOTOR), encoderRightMotor, RISING);
+  Timer1.initialize(100000);
+  Timer1.attachInterrupt(PID);
 }
-
 void loop() {
   nh.spinOnce();
-  if ((millis() - lastMilli) >= LOOPTIME)
-  {  
+  if ((millis() - lastMilli) >= LOOPTIME){  
     lastMilli = millis();
-
     if (abs(pos_left) < 5) {                                                  //Avoid taking in account small disturbances
       speed_act_left = 0;
     }
     else {
-      speed_act_left = ((pos_left / 480) * 2 * PI) * (1000 / LOOPTIME) * radius; // calculate speed of left wheel             !!!! 990 encoder total pulse coun
+      speed_act_left = ((pos_left / 1441) * 2 * PI) * (1 / SamplingTime) * radius; // Tính toán tốc độ thực tế của bánh xe trái(m/s)            !!!! 11*131 = 1441 encoder total pulse coun
     }
-
     if (abs(pos_right) < 5) {                                                 //Avoid taking in account small disturbances
       speed_act_right = 0;
     }
     else {
-      speed_act_right = ((pos_right / 480) * 2 * PI) * (1000 / LOOPTIME) * radius; // calculate speed of right wheel          !!!! 990 encoder total pulse coun
+      speed_act_right = ((pos_right / 1441) * 2 * PI) * (1 / SamplingTime) * radius; // Tính toán tốc độ thực tế của bánh xe bên(m/s)        !!!! 1441 encoder total pulse coun
     }
-
     encoder_left = encoder_left + pos_left;
     encoder_right = encoder_right + pos_right;
 
     if(encoder_left>32000) encoder_left = 0; 
     if(encoder_right>32000) encoder_right = 0; 
-    
-
     pos_left = 0;
     pos_right = 0;
-
-    speed_cmd_left = constrain(speed_cmd_left, -max_speed, max_speed);
-    PID_leftMotor.Compute();                                                 // compute PWM value for left motor
-    PWM_leftMotor = constrain(((speed_req_left+sgn(speed_req_left)*0.0882)/0.00235) + (speed_cmd_left/0.00235), -255, 255);
-
-    if (noCommLoops >= noCommLoopMax) {                   //Stopping if too much time without command
+  void PID(){
+    // PID cho động cơ trái
+  speed_act_left = (pos_left/(11*131)*(1/SamplingTime)*60);//rpm
+  pos_left = 0;
+  left_error = speed_req_left - speed_act_left;
+  aL = 2*SamplingTime*kpL + kiL*SamplingTime*SamplingTime + 2*kdL;
+  bL = kiL*SamplingTime*SamplingTime - 4*kdL - 2*SamplingTime*kpL;
+  gL = 2*kdL;
+  PWM_leftMotor = (aL*left_error + bL*left_lastError + gL*left_llastError + 2*SamplingTime*PWM_leftMotor_last)/(2*SamplingTime);
+  PWM_leftMotor_last = PWM_leftMotor;
+  left_llastError = left_lastError;
+  left_lastError = left_error;
+  if (noCommLoops >= noCommLoopMax){                   //Stopping if too much time without command
       analogWrite(PIN_L_PWM, 0);
     }
     else if (speed_req_left == 0) {                       //Stopping
@@ -182,11 +160,17 @@ void loop() {
       digitalWrite(PIN_L_IN1, LOW);
       digitalWrite(PIN_L_IN2, HIGH);
     }
-
-    speed_cmd_right = constrain(speed_cmd_right, -max_speed, max_speed);
-    PID_rightMotor.Compute();                                                 // compute PWM value for right motor
-    PWM_rightMotor = constrain(((speed_req_right+sgn(speed_req_right)*0.0882)/0.00235) + (speed_cmd_right/0.00235), -255, 255); 
-
+    //PID cho động cơ phải
+    speed_act_right = (pos_right/(11*131)*(1/SamplingTime)*60);
+    pos_right = 0;
+    right_error = speed_req_right - speed_act_right;
+    aR = 2*SamplingTime*kpR + kiR*SamplingTime*SamplingTime + 2*kdR;
+    bR = kiR*SamplingTime*SamplingTime - 4*kdR - 2*SamplingTime*kpR;
+    gR = 2*kdR;
+    PWM_rightMotor = (aR*right_error + bR*right_lastError + gR*right_llastError + 2*SamplingTime*PWM_rightMotor_last)/(2*SamplingTime);
+    PWM_rightMotor_last = PWM_rightMotor;
+    right_llastError = right_lastError;
+    right_lastError = right_error;
     if (noCommLoops >= noCommLoopMax) {                   //Stopping if too much time without command
       analogWrite(PIN_R_PWM, 0);
     }
@@ -203,6 +187,7 @@ void loop() {
       digitalWrite(PIN_R_IN1, LOW);
       digitalWrite(PIN_R_IN2, HIGH);
     }
+  }
     noCommLoops++;
     if (noCommLoops == 65535) {
       noCommLoops = noCommLoopMax;
@@ -210,7 +195,6 @@ void loop() {
     publishSpeed();   //Publish odometry on ROS topic
   }
 }
-
 //Publish function for odometry, uses a vector type message to send the data (message type is not meant for that but that's easier than creating a specific message type)
 void publishSpeed() {
   speed_msg.header.stamp = nh.now();      //timestamp for odometry data
@@ -218,7 +202,6 @@ void publishSpeed() {
   speed_msg.vector.y = speed_act_right;   //right wheel speed (in m/s)
   speed_msg.vector.z = LOOPTIME / 1000;   //looptime, should be the same as specified in LOOPTIME (in s)
   speed_pub.publish(&speed_msg);
-
 
   encoder_msg.x= encoder_left;
   encoder_msg.y= encoder_right;
@@ -231,19 +214,13 @@ void publishSpeed() {
   nh.spinOnce();
   nh.loginfo("Publishing odometry");
 }
-
-//Left motor encoder counter
+//Đếm xung encoder động cơ trái
 void encoderLeftMotor() {
   if (digitalRead(PIN_L_ENCOD_A_MOTOR) == digitalRead(PIN_L_ENCOD_B_MOTOR)) pos_left++;
   else pos_left--;
 }
-
-//Right motor encoder counter
+//Đếm xung encoder động cơ phải
 void encoderRightMotor() {
   if (digitalRead(PIN_R_ENCOD_A_MOTOR) == digitalRead(PIN_R_ENCOD_B_MOTOR)) pos_right--;
   else pos_right++;
-}
-
-template <typename T> int sgn(T val) {
-  return (T(0) < val) - (val < T(0));
 }
